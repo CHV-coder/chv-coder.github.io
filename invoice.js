133235c6930bafa2d915395edf7547e902bbef9c
CHV-coder <chv-biz@yandex.ru>
2025-02-06 16:38:05 +0500

    Update invoice.js

--- invoice.js
+++ invoice.js
@@ -183,7 +183,7 @@ function updateInvoice(row) {
         // row.Subtotal = row.Items.reduce((a, b) => a + b.Price * b.Quantity, 0);
         row.Subtotal = row.Items.Subtotal;
         // row.Total = row.Subtotal - ((row.Deduction || 0) + (row.Taxes || 0));
-        row.Total = row.Items.Subtotal+100;
+        row.Total = invoice.Deduction;
       } catch (e) {
         console.error(e);
       }
